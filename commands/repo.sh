d3f22bc219084f9474a11cfc80a33fea29307b04
Mark Tse <mark.tse@neverendingqs.com>
2021-10-17 21:15:07 -0400

    feat(commands/repo): add set branch-protection.

--- commands/repo.sh
+++ commands/repo.sh
@@ -3,11 +3,22 @@ set -eu
 
 # TODO: branch protection, editorconfig, sponsorship
 
+DIRNAME=$(dirname ${0})
+
 CMD=${1}
 SUBCMD=${2}
+PARAM_0=${3-main}
 
 function set() {
   case "${SUBCMD}" in
+
+  branch-protection)
+    gh api repos/{owner}/{repo}/branches/{branch}/protection \
+      --method PUT \
+      --field branch=${PARAM_0} \
+      --input ${DIRNAME}/payload/repo.branch-protection.json
+    ;;
+
   merge-button)
     gh api repos/{owner}/{repo} \
       --method PATCH \
@@ -16,6 +27,7 @@ function set() {
       --field allow_rebase_merge=false \
       --field delete_branch_on_merge=true
     ;;
+
   *)
     echo "Unsupported command \"${CMD} ${SUBCMD}\""
     ;;
