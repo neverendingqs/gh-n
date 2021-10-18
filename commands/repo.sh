81ddcc81e0ab487cd79c1fafbfb5f5dab158b434
Mark Tse <mark.tse@neverendingqs.com>
2021-10-17 20:54:24 -0400

    refactor(commands/repo): add subcommand for merge button options.

--- commands/repo.sh
+++ commands/repo.sh
@@ -3,18 +3,30 @@ set -eu
 
 # TODO: branch protection, editorconfig, sponsorship
 
-# TODO: setup subcommand, rename "standardize"
+CMD=${1}
+SUBCMD=${2}
 
-case "${1}" in
-standardize)
-  gh api repos/{owner}/{repo} \
-    --method PATCH \
-    --field allow_squash_merge=true \
-    --field allow_merge_commit=false \
-    --field allow_rebase_merge=false \
-    --field delete_branch_on_merge=true
+function set() {
+  case "${SUBCMD}" in
+  merge-button)
+    gh api repos/{owner}/{repo} \
+      --method PATCH \
+      --field allow_squash_merge=true \
+      --field allow_merge_commit=false \
+      --field allow_rebase_merge=false \
+      --field delete_branch_on_merge=true
+    ;;
+  *)
+    echo "Unsupported command \"${CMD} ${SUBCMD}\""
+    ;;
+  esac
+}
+
+case "${CMD}" in
+set)
+  set
   ;;
 *)
-  echo "Unsupported command ${1}"
+  echo "Unsupported command \"${CMD}\""
   ;;
 esac
